c8bf743d41a37293dc905421845da15576dd66eb
Ricardo Mendes <ricardo.mendes@ua.pt>
2015-05-13 00:43:22 +0200

    Chat - Init
    Node.js - Chat

---
+++ README.md
@@ -0,0 +1,29 @@
+Node.js - Chat
+================
+
+
+Getting started
+---------------
+
+
+Introduction
+------------
+
+- [Node.js](http://nodejs.org/)
+- [Socket.io](http://socket.io/)
+
+
+Requirements
+------------
+
+- Node.js :  [http://nodejs.org/](http://nodejs.org/)
+- Socket.io:  [http://socket.io/](http://socket.io/)
+
+
+Init Server
+------------
+
+node app.js
+
+(If don't work!)
+npm install
---
+++ public/js/myScript.js
@@ -0,0 +1,62 @@
+/**
+ * Created by ricardomendes on 12/05/15.
+ */
+var socket = io.connect('127.0.0.1:8080');
+
+var current_user ='';
+socket.on('connect', function () {
+    current_user = prompt("Nome?");
+    socket.emit('adduser', current_user);
+});
+
+socket.on('updatechat', function (color, username, data) {
+    if(username == current_user){
+        $('#conversation').append('<div class="chat-box-left">' + data + '</div><div class="chat-box-name-left"><div style="width:40px;height:40px;border-radius:40px;color:#fff;text-align:center;background:'+color+';display:inline-block;"> ' + username[0]+username[username.length-1] + '</div><div style="display:inline-block;padding: 0px 5px;color:'+color+'">- ' + username + '</div></div><hr class="hr-clas">'); 
+    } else{
+        $('#conversation').append('<div class="chat-box-right">' + data + '</div><div class="chat-box-name-right"><div style="width:40px;height:40px;border-radius:40px;color:#fff;text-align:center;background:'+color+';display:inline-block;"> ' + username[0]+username[username.length-1] + '</div><div style="display:inline-block;padding: 0px 5px;color:'+color+'">- ' + username + '</div></div><hr class="hr-clas">'); 
+    }
+    
+});
+
+socket.on('updaterooms', function (rooms, current_room) {
+    $('#numChats').empty();
+    $('#numChats').append("(" + rooms.length + ")");
+    $('#listChats').empty();    
+    $.each(rooms, function (key, value) {
+        if (value == current_room) {
+                $('#listChats').append('<a class="chat-box-online-left" href="#"><div style="width:40px;height:40px;border-radius:40px;color:#fff;text-align:center;background:#ff6a00;display:inline-block;"> <img class="img-circle" src="/images/room.png"></div><div style="display:inline-block;padding: 0px 5px;">- (' + value + ')</div>');            
+        }
+        else {
+            $('#listChats').append('<a class="chat-box-online-left" href="#" onclick="switchRoom(\'' + value + '\')"><div style="width:40px;height:40px;border-radius:40px;color:#fff;text-align:center;background:#ff6a00;display:inline-block;"> <img class="img-circle" src="/images/room.png"></div><div style="display:inline-block;padding: 0px 5px;">- (' + value + ')</div></a>');            
+        }
+        $('#listChats').append('<hr class="hr-clas-low">');
+    });
+});
+
+socket.on('updateNumOnline', function (numOnline, users) {
+    $('#numUsers').empty();
+    $('#numUsers').append("(" + numOnline + ")");
+    $('#listUsers').empty();    
+    $.each(users, function (key, value) { 
+        $('#listUsers').append('<div class="chat-box-online-users-left"><div style="width:40px;height:40px;border-radius:40px;color:#fff;text-align:center;background:' + value + ';display:inline-block;"> ' + key[0]+key[key.length-1] + '</div><div style="display:inline-block;padding: 0px 5px;color:' + value + '">- ' + key + '</div></div><hr class="hr-clas-low">');
+    });
+});
+
+function switchRoom(room) {
+    socket.emit('switchRoom', room);
+}
+
+$(function () {
+    $('#datasend').click(function () {
+        var message = $('#data').val();
+        $('#data').val('');
+        socket.emit('sendchat', message);
+    });
+
+    $('#data').keypress(function (e) {
+        if (e.which == 13) {
+            $(this).blur();
+            $('#datasend').focus().click();
+        }
+    });
+});
